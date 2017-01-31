0cccf9ffe00ea31182f0b487bf23d9377720f7ed
fenixsarov <fenixsarov@gmail.com>
2017-01-31 23:10:37 +0300

    Добавлены файлы шестого урока и внесены незначительные изменения в ДЗ №5

---
+++ 5/hometask_5/Citadel/Shop/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, render_to_response, HttpResponseRedirect
+from django.contrib import auth
+from django.http import Http404
+
+def main(request):
+    page = 'index'
+    return render_to_response("index.html", {"page": page})
+
+def login_page(request):
+    page = 'login_page'
+    return render_to_response("login.html", {"page": page})
+
+def login(request):
+
+    if request.method == 'POST':
+        print("POST data= {}".format(request.POST))
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, 'index.html', {'username': username, 'errors': True})
+    raise Http404
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/")
+# Create your views here.
