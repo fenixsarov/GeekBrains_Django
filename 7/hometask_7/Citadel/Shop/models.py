2a6fa457d75643ca68284ed286c8452bfc9edb61
fenixsarov <fenixsarov@gmail.com>
2017-02-07 19:52:01 +0300

    Незначительная правка

--- 7/hometask_7/Citadel/Shop/models.py
+++ 7/hometask_7/Citadel/Shop/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     name = models.CharField(verbose_name=u'Название', max_length=16, unique=True)
     description = models.TextField(verbose_name='Описание', blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Contact(models.Model):
     full_name = models.CharField(max_length=20)
 # Create your models here.
